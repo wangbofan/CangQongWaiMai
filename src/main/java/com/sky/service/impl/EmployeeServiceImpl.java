b47437e5cc926fdedb8fe7e533acac7aaa0bac4f
wang <2243785602@qq.com>
2025-06-22 15:28:39 +0800

    1.新增员工接口完善

--- src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++
@@ -1,7 +0,0 @@
-        // 检查用户名是否已存在
-        Employee existingEmployee = employeeMapper.getByUsername(employee.getUsername());
-        if (existingEmployee != null) {
-            throw new RuntimeException("用户名 '" + employee.getUsername() + "' 已经存在，请更换一个。");
-        }
-
-        employee.setStatus(StatusConstant.ENABLE);
