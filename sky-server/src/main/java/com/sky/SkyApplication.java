3fefbc9765135257760d10198d187d383be70f68
wang <2243785602@qq.com>
2025-06-23 15:57:32 +0800

    测试无用信息 回退1

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -12,5 +12,8 @@ public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
+
+//        远程回退1
+
     }
 }
